958faff860b81e9964b64af6cec722b952d918fb
MarinescuEduard <marinescu.eduard98@gmail.com>
2024-03-20 23:10:58 +0200

    Second commit

--- main.py
+++ main.py
@@ -44,63 +44,65 @@ for side in range(4):
 border_pen.hideturtle()
 
 # Create the ST turtle
-striker = turtle.Turtle()
-striker.color("blue")
-striker.shape("triangle")
-striker.penup()
-striker.speed(0)
-striker.setposition(0, -120)
-striker.setheading(90)
+strikerTurtle = turtle.Turtle()
+strikerTurtle.color("blue")
+strikerTurtle.shape("triangle")
+strikerTurtle.penup()
+strikerTurtle.speed(0)
+strikerTurtle.setposition(0, -120)
+strikerTurtle.setheading(90)
 
 # Create the GK turtle
-goal = turtle.Turtle()
-goal.color("red")
-goal.shape("circle")
-goal.penup()
-goal.speed(0)
-goal.setheading(-90)
-goal.setposition(0, 200)
+goalkeeperTurtle = turtle.Turtle()
+goalkeeperTurtle.color("red")
+goalkeeperTurtle.shape("circle")
+goalkeeperTurtle.penup()
+goalkeeperTurtle.speed(0)
+goalkeeperTurtle.setheading(-90)
+goalkeeperTurtle.setposition(0, 200)
 
 # Create the ball
-ball = turtle.Turtle()
-ball.color("yellow")
-ball.shape("circle")
-ball.penup()
-ball.speed(0)
-ball.setheading(90)
-ball.shapesize(0.3, 0.3)
-ball.setposition(0, -100)
+ballTurtle = turtle.Turtle()
+ballTurtle.color("yellow")
+ballTurtle.shape("circle")
+ballTurtle.penup()
+ballTurtle.speed(0)
+ballTurtle.setheading(90)
+ballTurtle.shapesize(0.3, 0.3)
+ballTurtle.setposition(0, -100)
 
 # Draw the result on the bottom
-conclusion = turtle.Turtle()
-conclusion.color("white")
-conclusion.penup()
-conclusion.speed(0)
-conclusion.hideturtle()
+conclusionTurtle = turtle.Turtle()
+conclusionTurtle.color("white")
+conclusionTurtle.penup()
+conclusionTurtle.speed(0)
+conclusionTurtle.hideturtle()
 
 # Draw the players on each side
-players = turtle.Turtle()
-players.color("white")
-players.speed(0)
-players.hideturtle()
+playersTurtle = turtle.Turtle()
+playersTurtle.color("white")
+playersTurtle.speed(0)
+playersTurtle.hideturtle()
 
 # Draw exit button
-stoppage = turtle.Turtle()
-stoppage.penup()
-stoppage.goto(250, 350)
-stoppage.shape("square")
-stoppage.shapesize(2)
-stoppage.fillcolor("red")
+stopTurtle = turtle.Turtle()
+stopTurtle.penup()
+stopTurtle.goto(250, 350)
+stopTurtle.shape("square")
+stopTurtle.shapesize(2)
+stopTurtle.fillcolor("red")
+# stopTurtle.write("Exit")
 # stoppage.write("Exit!", move=False, align="center", font=("Arial", 10, "normal"))
 
 
 # Draw new players button
-newplayers = turtle.Turtle()
-newplayers.penup()
-newplayers.goto(-250, 350)
-newplayers.shape("square")
-newplayers.shapesize(3)
-newplayers.fillcolor("blue")
+newPlayersTurtle = turtle.Turtle()
+newPlayersTurtle.penup()
+newPlayersTurtle.goto(-250, 350)
+newPlayersTurtle.shape("square")
+newPlayersTurtle.shapesize(3)
+newPlayersTurtle.fillcolor("blue")
+# newPlayersTurtle.write("Repeat")
 
 
 # newplayers.write("New\n", move=False, align="center", font=("Arial", 10, "normal"))
@@ -109,27 +111,27 @@ newplayers.fillcolor("blue")
 
 def resetTheGame():
     # Reset the ST turtle
-    striker.penup()
-    striker.speed(0)
-    striker.setposition(0, -120)
-    striker.setheading(90)
+    strikerTurtle.penup()
+    strikerTurtle.speed(0)
+    strikerTurtle.setposition(0, -120)
+    strikerTurtle.setheading(90)
     # Reset the GK turtle
-    goal.penup()
-    goal.speed(0)
-    goal.setheading(-90)
-    goal.setposition(0, 200)
+    goalkeeperTurtle.penup()
+    goalkeeperTurtle.speed(0)
+    goalkeeperTurtle.setheading(-90)
+    goalkeeperTurtle.setposition(0, 200)
     # Reset the ball
-    ball.penup()
-    ball.speed(0)
-    ball.setheading(90)
-    ball.shapesize(0.3, 0.3)
-    ball.setposition(0, -100)
-    players.reset()
-    players.penup()
-    players.hideturtle()
-    conclusion.reset()
-    conclusion.penup()
-    conclusion.hideturtle()
+    ballTurtle.penup()
+    ballTurtle.speed(0)
+    ballTurtle.setheading(90)
+    ballTurtle.shapesize(0.3, 0.3)
+    ballTurtle.setposition(0, -100)
+    playersTurtle.reset()
+    playersTurtle.penup()
+    playersTurtle.hideturtle()
+    conclusionTurtle.reset()
+    conclusionTurtle.penup()
+    conclusionTurtle.hideturtle()
 
 
 class GK:
@@ -168,91 +170,91 @@ def stAttributes(striker):
 
 def overallGK(gk):
     overall = 25 / 100 * gk.reflexes + 18 / 100 * gk.decision + 25 / 100 * gk.shotStopping + 18 / 100 * gk.anticipation + 14 / 100 * gk.balance
-    players.setposition(-450, -60)
-    players.write("Overall GK:" + str(round(overall)), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, -60)
+    playersTurtle.write("Overall GK:" + str(round(overall)), move=False, align="left", font=("Arial", 12, "normal"))
     return overall
 
 
 def overallST(st):
     overall = 15 / 100 * st.decision + 5 / 100 * st.flair + 23 / 100 * st.finishing + 23 / 100 * st.penalty + 17 / 100 * st.shooting + 17 / 100 * st.composure
-    players.setposition(330, -60)
-    players.write("Overall ST:" + str(round(overall)), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, -60)
+    playersTurtle.write("Overall ST:" + str(round(overall)), move=False, align="left", font=("Arial", 12, "normal"))
     return overall
 
 
 def printGK(gk):
-    players.setposition(-450, 40)
-    players.write("GK Reflexes:" + str(gk.reflexes), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(-450, 20)
-    players.write("GK Decisions:" + str(gk.decision), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(-450, 0)
-    players.write("GK Shot Stopping:" + str(gk.shotStopping), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(-450, -20)
-    players.write("GK Anticipation:" + str(gk.anticipation), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(-450, -40)
-    players.write("GK Balance:" + str(gk.balance), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, 40)
+    playersTurtle.write("GK Reflexes:" + str(gk.reflexes), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, 20)
+    playersTurtle.write("GK Decisions:" + str(gk.decision), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, 0)
+    playersTurtle.write("GK Shot Stopping:" + str(gk.shotStopping), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, -20)
+    playersTurtle.write("GK Anticipation:" + str(gk.anticipation), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(-450, -40)
+    playersTurtle.write("GK Balance:" + str(gk.balance), move=False, align="left", font=("Arial", 12, "normal"))
 
 
 def printST(st):
-    players.setposition(330, 60)
-    players.write("ST Decisions:" + str(st.decision), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(330, 40)
-    players.write("ST Flair:" + str(st.flair), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(330, 20)
-    players.write("ST Finishing:" + str(st.finishing), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(330, 0)
-    players.write("ST Penalty:" + str(st.penalty), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(330, -20)
-    players.write("ST Shooting" + str(st.shooting), move=False, align="left", font=("Arial", 12, "normal"))
-    players.setposition(330, -40)
-    players.write("ST Composure" + str(st.composure), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, 60)
+    playersTurtle.write("ST Decisions:" + str(st.decision), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, 40)
+    playersTurtle.write("ST Flair:" + str(st.flair), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, 20)
+    playersTurtle.write("ST Finishing:" + str(st.finishing), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, 0)
+    playersTurtle.write("ST Penalty:" + str(st.penalty), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, -20)
+    playersTurtle.write("ST Shooting" + str(st.shooting), move=False, align="left", font=("Arial", 12, "normal"))
+    playersTurtle.setposition(330, -40)
+    playersTurtle.write("ST Composure" + str(st.composure), move=False, align="left", font=("Arial", 12, "normal"))
 
 
 def rezultat(s1, s2):
-    conclusion.setposition(15, -300)
+    conclusionTurtle.setposition(15, -300)
     if s1 < s2:
         caz = random.randint(1, 3)
         if caz == 1:
-            goal.lt(90)
-            goal.fd(120)
-            ball.fd(330)
-            ball.lt(90)
-            ball.fd(100)
-            conclusion.write("No chance for the keeper. It is a goal!", move=False, align="center",
-                             font=("Arial", 28, "normal"))
+            goalkeeperTurtle.lt(90)
+            goalkeeperTurtle.fd(120)
+            ballTurtle.fd(330)
+            ballTurtle.lt(90)
+            ballTurtle.fd(100)
+            conclusionTurtle.write("No chance for the keeper. It is a goal!", move=False, align="center",
+                                   font=("Arial", 28, "normal"))
         elif caz == 2:
-            goal.lt(-90)
-            goal.fd(120)
-            ball.fd(320)
-            ball.lt(-90)
-            ball.fd(120)
-            conclusion.write("No chance for the keeper. It is a goal!", move=False, align="center",
-                             font=("Arial", 28, "normal"))
+            goalkeeperTurtle.lt(-90)
+            goalkeeperTurtle.fd(120)
+            ballTurtle.fd(320)
+            ballTurtle.lt(-90)
+            ballTurtle.fd(120)
+            conclusionTurtle.write("No chance for the keeper. It is a goal!", move=False, align="center",
+                                   font=("Arial", 28, "normal"))
         else:
-            ball.fd(340)
-            ball.lt(90)
-            ball.fd(100)
-            conclusion.write("No chance for the keeper. It is a goal!", move=False, align="center",
-                             font=("Arial", 28, "normal"))
+            ballTurtle.fd(340)
+            ballTurtle.lt(90)
+            ballTurtle.fd(100)
+            conclusionTurtle.write("No chance for the keeper. It is a goal!", move=False, align="center",
+                                   font=("Arial", 28, "normal"))
     else:
         caz = random.randint(1, 3)
         if caz == 1:
-            goal.lt(-90)
-            goal.fd(100)
-            ball.fd(300)
-            ball.lt(90)
-            ball.fd(100)
-            conclusion.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
+            goalkeeperTurtle.lt(-90)
+            goalkeeperTurtle.fd(100)
+            ballTurtle.fd(300)
+            ballTurtle.lt(90)
+            ballTurtle.fd(100)
+            conclusionTurtle.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
         elif caz == 2:
-            goal.lt(90)
-            goal.fd(100)
-            ball.fd(300)
-            ball.lt(-90)
-            ball.fd(100)
-            conclusion.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
+            goalkeeperTurtle.lt(90)
+            goalkeeperTurtle.fd(100)
+            ballTurtle.fd(300)
+            ballTurtle.lt(-90)
+            ballTurtle.fd(100)
+            conclusionTurtle.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
         else:
-            ball.fd(300)
-            conclusion.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
+            ballTurtle.fd(300)
+            conclusionTurtle.write("What a catch from the keeper!", move=False, align="center", font=("Arial", 28, "normal"))
 
 
 def letsPlay(x, y):
@@ -272,6 +274,6 @@ def letsStop(x, y):
     mainScreen.bye()
 
 
-newplayers.onclick(letsPlay)
-stoppage.onclick(letsStop)
+newPlayersTurtle.onclick(letsPlay)
+stopTurtle.onclick(letsStop)
 mainScreen.mainloop()
